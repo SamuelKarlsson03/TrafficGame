eb2c6326b3c7dbc9c3f503d6d2685e9395e9d528
Jenny Särnberger <113012121+Hinalina@users.noreply.github.com>
2023-09-04 13:54:30 +0200

    TestCar

---
+++ VRProject/Assets/Script/CarController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    [Header ("Movement")]
+    public float moveSpeed;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = transform.forward * moveSpeed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Wall") || other.CompareTag("Car"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}

1f4badacba31e400bef7efa69b30470783886a83
Jenny Särnberger <113012121+Hinalina@users.noreply.github.com>
2023-09-11 10:10:00 +0200

    game manager & new explosion

--- VRProject/Assets/Script/GameManager.cs
+++ VRProject/Assets/Script/GameManager.cs
@@ -1,20 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance;
+    public static GameManager manager;
 
     private void Awake()
     {
-        if(Instance == null)
+        if (manager == null)
         {
-            Instance = this;
+            manager = this;
+            DontDestroyOnLoad(this);
         }
-        else
+        else if (manager != this)
         {
             Destroy(gameObject);
         }
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
     }
 }

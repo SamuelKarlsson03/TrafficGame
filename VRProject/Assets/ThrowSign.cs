c7cba4b873cada4ef089ca15e8b1f105f24c5a99
Jenny Särnberger <jennysarnberger.work@gmail.com>
2023-09-25 15:10:58 +0200

    Början på sign throw thing

---
+++ VRProject/Assets/ThrowSign.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum State { Throw,Return,Idle }
+public class ThrowSign : MonoBehaviour
+{
+    public float signSpeed;
+    Rigidbody rb;
+    State state;
+
+    public Transform playerHand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = State.Idle;
+        rb = GetComponent<RigidBody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (State)
+        {
+            case State.Throw:
+                {
+                    rb.velocity = rb.veloctiry.normalized * signSpeed;
+                    Debug.Log(rb.velocity);
+                }
+
+                break;
+            case State.Return:
+                {
+                    Vector3 dir = playerHand.position - transform.position;
+                    rb.velocity = dir * signSpeed;
+                }
+
+                break;
+            case State.Idle:
+                {
+                    Debug.Log("Idle");
+                }
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void ThrowSigns()
+    {
+        state = State.Throw;
+    }
+
+    public void ReturnSigns()
+    {
+        state = State.Return;
+    }
+
+    public void IdleSigns()
+    {
+        state = State.Idle;
+    }
+}

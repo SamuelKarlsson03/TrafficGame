0af3f1412a4ab33b7a36386da017be0ef8f15ca8
Tom <tom.eckerstrom@hotmail.com>
2023-09-25 13:51:42 +0200

    Sign rotations

--- VRProject/Assets/SignControls.cs
+++ VRProject/Assets/SignControls.cs
@@ -1,37 +1,68 @@
 using UnityEngine;
 using Drawing;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class SignControls : MonoBehaviour
 {
-    [SerializeField] Vector3 boxCastSize;
-    [SerializeField] bool stopSign;
-    [SerializeField] float maxReach = 5f;
+    public XRDirectInteractor rHand;
+    public XRDirectInteractor lHand;
+
+    Vector3 direction;
+    int rotationDirection = 1;
+
+    Vector3 boxCastSize;
+    readonly float maxReach = 5f;
+
+    bool held = false;
 
     private void Start()
     {
-        //boxCastSize = GetComponent<BoxCollider>().size;
+        boxCastSize = GetComponent<BoxCollider>().size;
+        direction = transform.TransformDirection(Vector3.right * rotationDirection);
     }
 
     void FixedUpdate()
     {
-        Vector3 direction = transform.TransformDirection(Vector3.right);
-        RaycastHit hit;
-        if (Physics.BoxCast(transform.position, boxCastSize / 2, direction, out hit, transform.rotation, maxReach) 
-            || Physics.BoxCast(transform.position, boxCastSize / 2, -direction, out hit, transform.rotation, maxReach))
+        if (held)
         {
-            if (hit.collider.CompareTag("Car"))
+            if (Physics.BoxCast(transform.position, boxCastSize / 2, direction, out RaycastHit hit, transform.rotation, maxReach))
             {
-                Debug.Log("carhit");
-                hit.collider.GetComponent<Car>().stopping = stopSign;
+                if (hit.collider.CompareTag("Car"))
+                {
+                    Debug.Log("carhit");
+                    //Do Stuff
+                }
             }
         }
-
-        //Debug
     }
 
-    private void OnDrawGizmos()
+    public void PickUpSign(GameObject hand)
     {
-        Gizmos.color = Color.green;
-        Gizmos.DrawWireCube(transform.TransformPoint(Vector3.right * (maxReach / 2)), new Vector3(maxReach, boxCastSize.y, boxCastSize.z));
+        if (this.CompareTag(rHand.interactablesSelected[0].transform.gameObject.tag))
+            hand = rHand.gameObject;
+        else if (this.CompareTag(lHand.interactablesSelected[0].transform.gameObject.tag))
+            hand = lHand.gameObject;
+
+        // Get the forward direction of the hand (Z-axis).
+        Vector3 handForward = hand.transform.forward;
+
+        // Get the correct forward direction of the stop sign (X-axis).
+        Vector3 stopSignForward = transform.right; // Assuming stop sign's forward is along the X-axis.
+
+        // Calculate the dot product between the hand's forward direction and stop sign's forward direction.
+        float dotProduct = Vector3.Dot(handForward, stopSignForward);
+
+        // Determine the sign (1 or -1) based on the dot product.
+        rotationDirection = (dotProduct >= 0) ? 1 : -1;
+
+        direction = transform.TransformDirection(Vector3.right * rotationDirection);
+
+        held = true;
+    }
+
+    public void DropSign()
+    {
+        held = false;
     }
 }

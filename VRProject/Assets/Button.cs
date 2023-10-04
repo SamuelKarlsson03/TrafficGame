64b27de2e044aa2fa67078c97482def4a36e6f59
sandeeen <60133440+sandeeen@users.noreply.github.com>
2023-10-04 15:49:57 +0200

    Tutorial stuff

--- VRProject/Assets/Button.cs
+++ VRProject/Assets/Button.cs
@@ -5,14 +5,84 @@ using UnityEngine.SceneManagement;
 
 public class Button : MonoBehaviour
 {
-  
 
-    private void OnCollisionEnter(Collision collision)
+    [SerializeField] GameObject leftHand;
+    [SerializeField] GameObject rightHand;
+    [SerializeField] Vector3 offset;
+    [SerializeField] float range;
+    [SerializeField] Transform topPos;
+    [SerializeField] Transform bottomPos;
+    [SerializeField] float riseSpeed;
+
+    [SerializeField] bool isResetButton = false;
+
+    [SerializeField] bool isMoveonButton = false;
+
+    private void Start()
     {
-        if (collision.gameObject.CompareTag("Button"))
+
+    }
+
+
+
+    private void Update()
+    {
+
+        float leftHeight = -1000f;
+        float rightHeight = -1000f;
+
+
+        if (Vector3.Distance(leftHand.transform.position, transform.position) < range)
+        {
+            leftHeight = leftHand.transform.position.y + offset.y;
+        }
+
+        if (Vector3.Distance(rightHand.transform.position, transform.position) < range)
+        {
+            rightHeight = rightHand.transform.position.y + offset.y;
+        }
+        bool shouldRise = true;
+        if (leftHeight + rightHeight > -1999f)
+        {
+            float y = 0;
+             
+            if (leftHeight > rightHeight)
+            {
+                y = Mathf.Clamp(leftHeight, bottomPos.position.y, topPos.position.y);
+            }
+
+            if (rightHeight > leftHeight)
+            {
+                y = Mathf.Clamp(rightHeight, bottomPos.position.y, topPos.position.y);
+            }
+            if(y <= transform.position.y)
+            {
+                shouldRise = false;
+                if(y <= bottomPos.position.y)
+                {
+                    ButtonPressed();
+                }
+                transform.position = new Vector3(transform.position.x, y, transform.position.z);
+            }
+        }
+        if(shouldRise)
+        {
+            transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y + riseSpeed * Time.deltaTime, bottomPos.position.y, topPos.position.y), transform.position.z);
+        }
+
+    }
+
+    public void ButtonPressed()
+    {
+        if (isResetButton)
         {
             SceneManager.LoadScene("Intro");
+        }
 
+        if (isMoveonButton)
+        {
+            SceneManager.LoadScene("GameScene");
         }
     }
+
 }

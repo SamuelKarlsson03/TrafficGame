f0509a3249339b7754bcb9a0dbc4949bf9f2f6c1
Samuel Karlsson <samuel.karlsson_03@hotmail.com>
2023-10-05 17:04:26 +0200

    text faces player hopefully

--- VRProject/Assets/Script/TextAnimate.cs
+++ VRProject/Assets/Script/TextAnimate.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
 
 public class TextAnimate : MonoBehaviour
 {
     [SerializeField] float floatSpeed;
+    Transform playerposition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerposition = GameObject.Find("Left Controller").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += Vector3.up * Time.deltaTime * floatSpeed;
+        transform.forward = (transform.position-playerposition.position).normalized;
     }
 }
